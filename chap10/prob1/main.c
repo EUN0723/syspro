3780dc33ae9918c5156dddb7e780b9266196d429
yeoung eun <cye7066@cu.ac.kr>
2023-11-30 06:02:35 +0000

    1130

--- chap10/prob1/main.c
+++ chap10/prob1/main.c
@@ -26,7 +26,7 @@ ptr = (struct student *) malloc(n * sizeof(struct student));
 	
 	 printf("Please enter the student number and name of %d\n", n);
 	 for (i = 0; i < n; i++)
-                 scanf("%d %s\n", &ptr[i].id, ptr[i].name);
+                 scanf("%d %s", &ptr[i].id, ptr[i].name);
 	
 	 printf("\n* Student Information (reverse) *\n");
 	 for (i = n-1; i >= 0; i--)
